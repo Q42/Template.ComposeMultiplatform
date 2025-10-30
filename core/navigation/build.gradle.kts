e3d5dc32ec9d606fff32516d5b009f3eb713c6e5
Frank1234 <frankf@q42.nl>
2025-10-30 11:12:11 +0100

    ADD jvm target to all modules

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -28,6 +28,8 @@ kotlin {
     // https://developer.android.com/kotlin/multiplatform/migrate
     val xcfName = "core:navigationKit"
 
+    jvm()
+
     iosArm64 {
         binaries.framework {
             baseName = xcfName
