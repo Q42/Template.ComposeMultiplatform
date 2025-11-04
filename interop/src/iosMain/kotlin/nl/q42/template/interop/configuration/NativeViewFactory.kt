0775886d3e0cedef2d8a870c3262a9a160c18ebe
Marcel Bloemendaal <marcel.bloemendaal@q42.nl>
2025-11-04 17:02:55 +0100

    remove non-working annotation

--- interop/src/iosMain/kotlin/nl/q42/template/interop/configuration/NativeViewFactory.kt
+++ interop/src/iosMain/kotlin/nl/q42/template/interop/configuration/NativeViewFactory.kt
@@ -1,10 +1,7 @@
 package nl.q42.template.interop.configuration
 
 import platform.UIKit.UIViewController
-import kotlin.experimental.ExperimentalObjCName
 
-@OptIn(ExperimentalObjCName::class)
-@ObjCName(swiftName = "NativeViewFactory")
 interface NativeViewFactory {
     fun createButton(text: String, onClick: () -> Unit): UIViewController
 }
