34b9a588a5a7a31ba9161a8f817cf3382ab3a336
Marcel Bloemendaal <marcel.bloemendaal@q42.nl>
2025-11-06 10:11:30 +0100

    remove: redundant TODO

--- interop/src/jvmMain/kotlin/nl/q42/template/interop/JvmNativeDependencyExample.kt
+++ interop/src/jvmMain/kotlin/nl/q42/template/interop/JvmNativeDependencyExample.kt
@@ -2,7 +2,6 @@ package nl.q42.template.interop
 
 import kotlinx.coroutines.delay
 
-// TODO: Implement any JVM-specific native functionality here
 class JvmNativeDependencyExample: NativeDependencyExample {
     override fun executeNativeMethod() {
         println("Hello from JVM!")
