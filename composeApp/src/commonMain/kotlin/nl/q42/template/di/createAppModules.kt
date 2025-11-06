3f2baee72854fdbed8717c13ec4cea6986aad222
Marcel Bloemendaal <marcel.bloemendaal@q42.nl>
2025-11-06 10:10:17 +0100

    remove / fix tests

--- composeApp/src/commonMain/kotlin/nl/q42/template/di/createAppModules.kt
+++ composeApp/src/commonMain/kotlin/nl/q42/template/di/createAppModules.kt
@@ -11,6 +11,7 @@ import nl.q42.template.domain.main.usecase.FetchUserUseCase
 import nl.q42.template.domain.main.usecase.GetUserFlowUseCase
 import nl.q42.template.feature.home.di.homeModule
 import nl.q42.template.feature.onboarding.di.onboardingModule
+import nl.q42.template.interop.NativeDependencyExample
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
@@ -18,7 +19,7 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 @OptIn(KoinExperimentalAPI::class)
-val appModules = module {
+fun createAppModules(nativeDependencyExample: NativeDependencyExample) = module {
 
     includes(mainDataModule)
     includes(presentationModule)
@@ -32,4 +33,5 @@ val appModules = module {
     factoryOf(::ExecuteNativeAsyncExampleMethodUseCase)
 
     singleOf(::MyRouteNavigator) { bind<RouteNavigator>() }
+    single { nativeDependencyExample }
 }
--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,10 +1,8 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import nl.q42.template.App
-import nl.q42.template.di.appModules
-import nl.q42.template.interop.NativeDependencyExample
+import nl.q42.template.di.createAppModules
 import nl.q42.template.interop.configuration.IosAppConfiguration
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
 import platform.UIKit.UIViewController
 
 fun MainViewController(
@@ -13,10 +11,7 @@ fun MainViewController(
 
     startKoin {
         modules(
-            appModules,
-            module {
-                single<NativeDependencyExample> { iosAppConfiguration.nativeDependencyExample }
-            }
+            createAppModules(iosAppConfiguration.nativeDependencyExample)
         )
     }
 
