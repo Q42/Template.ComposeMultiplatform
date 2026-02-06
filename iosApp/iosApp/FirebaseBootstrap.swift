52f098fa6daba2cd404155d74ad74f54bb3ebbe0
Nikitina Kseniia <kseniia.nikitina@q42.nl>
2026-02-06 13:08:59 +0100

    ADD isDebug var

--- iosApp/iosApp/FirebaseBootstrap.swift
+++ iosApp/iosApp/FirebaseBootstrap.swift
@@ -6,6 +6,7 @@ import Foundation
 final class FirebaseBootstrap {
     func configure() {
         let isUIPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        let isDebug = _isDebugAssertConfiguration()
         let crashlyticsEnabled = !isUIPreview && !isDebug
 
         FirebaseApp.configure()
