9bb1beabb9d5403c3335b296582e0d6848a30463
Frank1234 <frankf@q42.nl>
2026-03-23 14:12:16 +0100

    CHANGE gradle change

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.android.build.api.dsl.androidLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import com.codingfeline.buildkonfig.compiler.FieldSpec
 import org.jetbrains.compose.reload.gradle.ComposeHotRun
