801b2a1db6256baa98b05448e7f5f15d4c0357bf
Frank <frankf@q42.nl>
2026-03-23 13:58:05 +0100

    Update composeApp/src/commonMain/kotlin/nl/q42/template/navigation/NavEntryForKey.kt
    Co-authored-by: Sebas LG <sebastian@q42.nl>

--- composeApp/src/commonMain/kotlin/nl/q42/template/navigation/NavEntryForKey.kt
+++ composeApp/src/commonMain/kotlin/nl/q42/template/navigation/NavEntryForKey.kt
@@ -12,37 +12,39 @@ import org.koin.core.parameter.parametersOf
 
 fun navEntryForKey(key: NavKey, navigator: Navigator) =
     when (key) {
-        Destination.Home -> {
-            NavEntry(key) {
-                HomeScreen(
-                    viewModel = koinViewModel(
-                        parameters = {
-                            parametersOf(navigator)
-                        }
+        is Destination -> when(key) {
+            Destination.Home -> {
+                NavEntry(key) {
+                    HomeScreen(
+                        viewModel = koinViewModel(
+                            parameters = {
+                                parametersOf(navigator)
+                            }
+                        )
                     )
-                )
+                }
             }
-        }
-        Destination.Onboarding -> {
-            NavEntry(key) {
-                OnboardingScreen(
-                    viewModel = koinViewModel(
-                        parameters = {
-                            parametersOf(navigator)
-                        }
+            Destination.Onboarding -> {
+                NavEntry(key) {
+                    OnboardingScreen(
+                        viewModel = koinViewModel(
+                            parameters = {
+                                parametersOf(navigator)
+                            }
+                        )
                     )
-                )
+                }
             }
-        }
-        Destination.InteropExamples -> {
-            NavEntry(key) {
-                InteropExamplesScreen(
-                    viewModel = koinViewModel(
-                        parameters = {
-                            parametersOf(navigator)
-                        }
+            Destination.InteropExamples -> {
+                NavEntry(key) {
+                    InteropExamplesScreen(
+                        viewModel = koinViewModel(
+                            parameters = {
+                                parametersOf(navigator)
+                            }
+                        )
                     )
-                )
+                }
             }
         }
         else -> error("Unknown NavKey: $key")
