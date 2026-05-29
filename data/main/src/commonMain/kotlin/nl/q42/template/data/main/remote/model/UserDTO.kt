2963aa8dabc5d5a5624c46f5b8169f91c3e06e25
Nikitina Kseniia <kseniia.nikitina@q42.nl>
2026-05-29 17:22:19 +0200

    CHANGE remove entities

--- data/main/src/commonMain/kotlin/nl/q42/template/data/main/remote/model/UserDTO.kt
+++ data/main/src/commonMain/kotlin/nl/q42/template/data/main/remote/model/UserDTO.kt
@@ -1,10 +1,10 @@
 package nl.q42.template.data.main.remote.model
 
 import kotlinx.serialization.Serializable
-import nl.q42.template.data.main.local.model.UserEntity
-import nl.q42.template.data.main.local.model.UserNameEntity
+import nl.q42.template.domain.main.model.User
+import nl.q42.template.domain.main.model.UserName
 
 @Serializable
 data class UserDTO(val title: String)
 
-internal fun UserDTO.toUserEntity(): UserEntity = UserEntity(name = UserNameEntity(value = title))
+internal fun UserDTO.toUser(): User = User(name = UserName(value = title))
