c97dc7cbb6acf543ffe52422b31ef3ace2035acc
Kseniia Nikitina <kseniia.nikitina@q42.nl>
2026-06-09 11:39:22 +0200

    Better approach for Debug mode and Config values (#149)

--- composeApp/src/commonMain/kotlin/nl/q42/template/logging/LoggerBootstrap.kt
+++ composeApp/src/commonMain/kotlin/nl/q42/template/logging/LoggerBootstrap.kt
@@ -3,16 +3,16 @@ package nl.q42.template.logging
 import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
-import nl.q42.template.BuildKonfig
 
 object LoggerBootstrap {
     fun initialize(
+        isDebug: Boolean,
         logWriter: LogWriter,
         crashReporter: CrashReporter,
     ) {
         Logger.setTag("Template")
 
-        if (BuildKonfig.DEBUG) {
+        if (isDebug) {
             Logger.setMinSeverity(Severity.Verbose)
             Logger.setLogWriters(logWriter)
 
