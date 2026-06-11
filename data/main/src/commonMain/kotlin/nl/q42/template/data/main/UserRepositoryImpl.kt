5b203af4180b3d0aa83eb7884a4af590cbda8c30
Kseniia Nikitina <kseniia.nikitina@q42.nl>
2026-06-11 11:53:11 +0200

    Remove Entities layer (#165)

--- data/main/src/commonMain/kotlin/nl/q42/template/data/main/UserRepositoryImpl.kt
+++ data/main/src/commonMain/kotlin/nl/q42/template/data/main/UserRepositoryImpl.kt
@@ -1,12 +1,10 @@
 package nl.q42.template.data.main
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import nl.q42.template.core.actionresult.model.ApiResult
 import nl.q42.template.core.actionresult.getDataOrNull
 import nl.q42.template.core.actionresult.map
+import nl.q42.template.core.actionresult.model.ApiResult
 import nl.q42.template.data.main.local.UserLocalDataSource
-import nl.q42.template.data.main.local.model.mapToUser
 import nl.q42.template.data.main.remote.UserRemoteDataSource
 import nl.q42.template.domain.main.model.User
 import nl.q42.template.domain.main.repo.UserRepository
@@ -19,17 +17,17 @@ internal class UserRepositoryImpl(
     override suspend fun fetchUser(): ApiResult<Unit> {
 
         // get remotely
-        val userEntityActionResult = userRemoteDataSource.getUser()
+        val userActionResult = userRemoteDataSource.getUser()
         // store locally
-        userEntityActionResult.getDataOrNull()?.let { userEntity ->
-                userLocalDataSource.setUser(userEntity)
+        userActionResult.getDataOrNull()?.let { user ->
+            userLocalDataSource.setUser(user)
         }
         // no need handle error case, we already do that in the remote data source
 
         // we send back unit, the user needs to be observed
-        return userEntityActionResult.map { }
+        return userActionResult.map { }
     }
 
     override fun getUserFlow(): Flow<User?> =
-        userLocalDataSource.getUserFlow().map { it?.mapToUser() }
+        userLocalDataSource.getUserFlow()
 }
