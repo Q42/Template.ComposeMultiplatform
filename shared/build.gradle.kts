49866bc6c7293a3d29406020e23f019e07b42dc9
eskil-q42 <eskil.sviggum@q42.nl>
2026-08-21 12:57:26 +0200

    Add plugin to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.buildKonfig)
     id("app.cash.licensee")
+    alias(libs.plugins.koin.compiler)
 }
 
 kotlin {
@@ -69,6 +70,7 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+            implementation(libs.koin.annotations)
             implementation(libs.coil)
             implementation(libs.coil.network.ktor)
             implementation(libs.kotlinx.datetime)
@@ -137,6 +139,10 @@ room {
     schemaDirectory("$projectDir/schemas")
 }
 
+koinCompiler {
+    // False-positives are detected on iOS. Remove when no longer the case
+    compileSafety = false
+}
 
 tasks.register("printAppVersionMetadata") {
     group = "help"
