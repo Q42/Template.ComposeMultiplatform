3111614de0c581ec50daaf08e146f5dda0e54dae
Nikitina Kseniia <kseniia.nikitina@q42.nl>
2026-09-04 17:43:45 +0200

    FIX xcfName fix

--- feature/samplehome/build.gradle.kts
+++ feature/samplehome/build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
     // A step-by-step guide on how to include this library in an XCode
     // project can be found here:
     // https://developer.android.com/kotlin/multiplatform/migrate
-    val xcfName = "feature:sampleonboardingKit"
+    val xcfName = "feature:samplehomeKit"
 
     iosArm64 {
         binaries.framework {
--- feature/sampleonboarding/build.gradle.kts
+++ feature/sampleonboarding/build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
     // A step-by-step guide on how to include this library in an XCode
     // project can be found here:
     // https://developer.android.com/kotlin/multiplatform/migrate
-    val xcfName = "feature:samplehomeKit"
+    val xcfName = "feature:sampleonboardingKit"
 
     iosArm64 {
         binaries.framework {
