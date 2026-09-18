72d444e2ccd2d6ebc927348f3167874017bbc7bf
Tom Friederich <tfriederich@live.nl>
2026-09-18 14:17:08 +0200

    CHANGE clarify that a viewStateString in a viewstate as param is OK
    feature/consistent-viewstatestring-usage

--- core/ui/src/commonMain/kotlin/nl/q42/template/core/ui/presentation/ViewStateString.kt
+++ core/ui/src/commonMain/kotlin/nl/q42/template/core/ui/presentation/ViewStateString.kt
@@ -11,8 +11,9 @@ import org.jetbrains.compose.resources.StringResource
  * There is one edge case: the formatArgs are not refreshed on config changes, so if you use a string with a
  * replacement parameter that is f.e. a localized date string, move your logic to the view instead of using [ViewStateString].
  *
- * Guideline: Only use ViewStateString when needed. Keep UI components 'Blissfully unaware' of the string source: just pass resolved strings to them.
- * (If you can use `stringResource` because you are in a composable context, you probably don't need [ViewStateString]).
+ * Guideline: Only use ViewStateString when needed. UI components should either receive strings directly, or a ViewState,
+ * (but that ViewState that may still may contain a ViewStateString if needed).
+ * (If you can simply use the native `stringResource` because you are in a composable context, you probably don't need [ViewStateString]).
  */
 sealed class ViewStateString {
     data class Res(
